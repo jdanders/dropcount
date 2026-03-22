17282b52517d5fdf664e1a9db5376ac2469ecfac
jdanders <jordan.the.anderson@gmail.com>
2026-03-22 08:44:28 -0600

    Drop back to API 35 and remove unneeded permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "io.github.jdanders.dropcount"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.jdanders.dropcount"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
